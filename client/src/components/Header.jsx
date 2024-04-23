5553bb5162c282a9bb9671bc95e93514c32ca5af
syazah <thesyazah@gmail.com>
2024-04-23 15:28:20 +0530

    Created Header

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,54 @@
+import { Button, Navbar, TextInput } from "flowbite-react";
+import { Link, useLocation } from "react-router-dom";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon } from "react-icons/fa";
+function Header() {
+  const path = useLocation().pathname;
+  return (
+    <Navbar className="border-b-2">
+      <Link
+        to={"/"}
+        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white text-white"
+      >
+        <span className="px-2 py-1 bg-gradient-to-b from-indigo-600 via-purple-400 to-blue-500 rounded-xl">
+          Azaan&apos;s Blog
+        </span>
+      </Link>
+
+      <form>
+        <TextInput
+          type="text"
+          placeholder="Search"
+          rightIcon={AiOutlineSearch}
+          className="hidden lg:inline-flex"
+        />
+      </form>
+      <Button className="w-12 h-10 lg:hidden" color="gray">
+        <AiOutlineSearch />
+      </Button>
+
+      <div className="flex gap-2 md:order-2">
+        <Button className="w-12 h-10 hidden sm:inline" color={"gray"} pill>
+          <FaMoon />
+        </Button>
+        <Link to={"/sign-in"}>
+          <Button gradientDuoTone={"purpleToBlue"}>Sign In</Button>
+        </Link>
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse>
+        <Navbar.Link active={path === "/"} as={"div"}>
+          <Link to={"/"}>HOME</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/about"} as={"div"}>
+          <Link to={"/about"}>ABOUT</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/projects"} as={"div"}>
+          <Link to={"/projects"}>PROJECTS</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
+
+export default Header;
