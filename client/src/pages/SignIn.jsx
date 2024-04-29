9349a43dbf324b8f1fefd088593878ebd0419126
syazah <thesyazah@gmail.com>
2024-04-29 13:07:25 +0530

    Sign In Complete

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,5 +1,114 @@
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
 function SignIn() {
-  return <div className="min-h-screen">SIGNIN</div>;
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  function handleChange(e) {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.email || !formData.password) {
+      return setErrorMessage("Please Fill Out All The Fields");
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if (res.ok) {
+        return navigate("/");
+      }
+    } catch (err) {
+      setErrorMessage(err.message);
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="min-h-screen mt-20">
+      <div className=" flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+        <div className="flex-1">
+          {" "}
+          <Link to={"/"} className="sm:text-xl font-bold  text-4xl">
+            <span className="px-2 py-1 bg-gradient-to-b from-indigo-600 via-purple-400 to-blue-500 rounded-xl text-white">
+              Azaan&apos;s
+            </span>
+            Blog
+          </Link>
+          <p>
+            This is a demo project Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Repellendus laborum asperiores temporibus tempore!
+            Fugiat veritatis tempore doloribus eveniet odit delectus itaque
+            fuga, consectetur sapiente totam porro nisi quis, neque minus.
+          </p>
+        </div>
+        <div className="flex-1">
+          <form className="flex flex-col gap-2" onSubmit={handleSubmit}>
+            <div>
+              <Label value="Your Email" />
+              <TextInput
+                type="email"
+                placeholder="name@company.com"
+                id="email"
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <Label value="Your Password" />
+              <TextInput
+                type="password"
+                placeholder="*********"
+                id="password"
+                onChange={handleChange}
+              />
+            </div>
+            <Button
+              disabled={loading}
+              className="mt-4"
+              gradientDuoTone={"purpleToPink"}
+              type="submit"
+            >
+              {loading ? (
+                <>
+                  <span>Loading ...</span>
+                  <Spinner />
+                </>
+              ) : (
+                "Sign Up"
+              )}
+            </Button>
+          </form>
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Don&apos;t Have an account ?</span>
+            <Link to="/sign-in" className="text-blue-500 underline">
+              Sign In
+            </Link>
+          </div>
+
+          {errorMessage && (
+            <Alert className="mt-5" color={"failure"}>
+              {errorMessage}
+            </Alert>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SignIn;
